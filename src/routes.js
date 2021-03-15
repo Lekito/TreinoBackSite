2bb411e9779c26db0b5b69e4af7b45f322a678fb
Alex Ribeiro Alves <alexlekito075@gmail.com>
2021-03-15 17:14:46 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,21 @@
 //const { Router} = require('express');
 import { Router} from 'express';
 
+import mongoose from 'mongoose';
+
 const routes = new Router();
+
+// Conexão com o banco de dados
+mongoose.connect('mongodb://localhost/alex', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+}).then(() => {
+    console.log("Conexão com MongoDB realizada com sucesso!");
+}).catch((erro) => {
+    console.log("Erro: Conexão com o MongoDB não foi realizada com sucesso! " + erro);
+});
 
 routes.get('/', (require, response) => {
     response.send("Alex");
