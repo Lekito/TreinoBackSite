0ae66fdf460ae60515738491263e1a0bf074db94
Alex Ribeiro Alves <alexlekito075@gmail.com>
2021-03-18 16:50:14 -0300

    Update UserController.js
    Validando os campos do formulário.

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,8 +2,52 @@ import User from '../models/User';
 
 class UserController{
     async store(require, response){
-        const user = await User.create(require.body);
-        return response.json(user);
+        const emailExiste = await User.findOne({email: require.body.email})
+        if(emailExiste){
+            return response.status(400).json({
+                error: true,
+                code: 102,
+                message: "Error: Este e-mail já está cadastrado! Tente outro e-mail!"
+            });
+        }
+
+        if(!require.body.name || require.body.name == undefined || require.body.name == null){
+            return response.status(400).json({
+                error: true,
+                code: 103,
+                message: "Error: Necessário informar o nome!"
+            });
+        }
+
+        if(!require.body.email || require.body.email == undefined || require.body.email == null){
+            return response.status(400).json({
+                error: true,
+                code: 104,
+                message: "Error: Necessário informar o e-mail!"
+            });
+        }
+
+        if(!require.body.password || require.body.password == undefined || require.body.password == null){
+            return response.status(400).json({
+                error: true,
+                code: 105,
+                message: "Error: Necessário informar senha!"
+            });
+        }
+
+        const user = await User.create(require.body, (error) => {
+            if(error) return response.status(400).json({
+                error: true,
+                code: 101,
+                message: "Error: Usuário não foi cadastrado com sucesso!"
+            });
+
+            return response.status(200).json({
+                error: false,
+                message: "Usuario cadastrado com sucesso!",
+                dados: user
+            })
+        });
     }
 }
 
