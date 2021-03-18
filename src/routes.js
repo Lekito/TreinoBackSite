b996429665b5cd3f3e1bb0736208e6e43c0b7419
Alex Ribeiro Alves <alexlekito075@gmail.com>
2021-03-18 15:33:22 -0300

    Update routes.js
    Criei a model para cadastrar no banco de dados e usando para cadastrar o usuário com método Get.

--- src/routes.js
+++ src/routes.js
@@ -1,27 +1,22 @@
-//const { Router} = require('express');
-import { Router} from 'express';
+import { response, Router} from 'express';
+import User from './app/models/User';
 
 import mongoose from 'mongoose';
 
 const routes = new Router();
 
-// Conexão com o banco de dados
-/*
-mongoose.connect('mongodb://localhost/alex', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true
-}).then(() => {
-    console.log("Conexão com MongoDB realizada com sucesso!");
-}).catch((erro) => {
-    console.log("Erro: Conexão com o MongoDB não foi realizada com sucesso! " + erro);
+routes.get('/', async(require, response) => {
+    await User.create( {
+        nome: 'Alan',
+        email: 'alan_eng@gmail.com',
+        senha: '123456a'
+    }, function(erro, small){
+        if(erro) return response.status(400).json({error: "Erro: Usuário não foi cadastrado com sucesso!"});
+        
+        return response.status(200).json({error: "Usuário cadastrado com sucesso!"}); 
+    });
 });
-*/
 
-routes.get('/', (require, response) => {
-    response.send("Alex");
-})
 
 routes.get('/contatos', (require, response) => {
     response.send("Alex, Alan, Vitor e Vinicius");
