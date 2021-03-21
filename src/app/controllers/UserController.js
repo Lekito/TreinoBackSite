21a00b5a6b1301d85b9bff2b216e19c582d6f07d
Alex Ribeiro Alves <alexlekito075@gmail.com>
2021-03-21 19:50:02 -0300

    Update UserController.js
    Validando no Node.js usando o Yup

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,37 +1,29 @@
+import * as yup from 'yup';
 import User from '../models/User';
 
 class UserController{
     async store(require, response){
+
+        const schema = yup.object().shape({
+            name: yup.string().required(),
+            email: yup.string().email().required(),
+            password: yup.string().required().min(6)
+        })
+
+        if(!(await schema.isValid(require.body))){
+            return response.status(400).json({
+                error: true,
+                code: 103,
+                message: "Error: Dados inválidos!"
+            });
+        }
+
         const emailExiste = await User.findOne({email: require.body.email})
         if(emailExiste){
             return response.status(400).json({
                 error: true,
                 code: 102,
                 message: "Error: Este e-mail já está cadastrado! Tente outro e-mail!"
-            });
-        }
-
-        if(!require.body.name || require.body.name == undefined || require.body.name == null){
-            return response.status(400).json({
-                error: true,
-                code: 103,
-                message: "Error: Necessário informar o nome!"
-            });
-        }
-
-        if(!require.body.email || require.body.email == undefined || require.body.email == null){
-            return response.status(400).json({
-                error: true,
-                code: 104,
-                message: "Error: Necessário informar o e-mail!"
-            });
-        }
-
-        if(!require.body.password || require.body.password == undefined || require.body.password == null){
-            return response.status(400).json({
-                error: true,
-                code: 105,
-                message: "Error: Necessário informar senha!"
             });
         }
 
