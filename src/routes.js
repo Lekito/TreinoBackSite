926e7c102fe499d28390ab36d98886da56934e34
Alex Ribeiro Alves <alexlekito075@gmail.com>
2021-03-21 22:45:44 -0300

    Update routes.js
    inclusão da rota para o login.

--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,13 @@
 import { response, Router} from 'express';
-import User from './app/models/User';
 
-import mongoose from 'mongoose';
 import UserController from './app/controllers/UserController';
+
+import LoginController from'./app/controllers/LoginController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
-
-/*routes.get('/', async(require, response) => {
-    await User.create( {
-        nome: 'Alan',
-        email: 'alan_eng@gmail.com',
-        senha: '123456a'
-    }, function(erro, small){
-        if(erro) return response.status(400).json({error: "Erro: Usuário não foi cadastrado com sucesso!"});
-        
-        return response.status(200).json({error: "Usuário cadastrado com sucesso!"}); 
-    });
-});
-*/
+routes.post('/login', LoginController.store)
 
 
 routes.get('/contatos', (require, response) => {
