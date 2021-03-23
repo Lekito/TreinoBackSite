f98eee7b0aba951b7efd7813f9c1f54576cc2085
Alex Ribeiro Alves <alexlekito075@gmail.com>
2021-03-23 15:25:50 -0300

    Update routes.js
    Adicionando Middleware para acessar o delete.

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,12 @@ import { response, Router} from 'express';
 import UserController from './app/controllers/UserController';
 
 import LoginController from'./app/controllers/LoginController';
+import authMiddlewares from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
-routes.delete('/users/:id', UserController.delete);
+routes.delete('/users/:id', authMiddlewares, UserController.delete);
 routes.post('/login', LoginController.store)
 
 
