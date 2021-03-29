09c1bd1747f5a2fb1cef8c8459b6d4609a291cbe
Alex Ribeiro Alves <alexlekito075@gmail.com>
2021-03-29 16:11:07 -0300

    Update routes.js
    Rota para upload da imagem e suas bibliotecas para seu funcionamento.

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,18 @@
 import { response, Router} from 'express';
+import multer from 'multer';
+import multerUpImgUsers from './app/middlewares/uploadImgUser';
 
 import UserController from './app/controllers/UserController';
 import PerfilController from './app/controllers/PerfilController';
+import PerfilImagemController from './app/controllers/PerfilImagemController';
 
 import LoginController from'./app/controllers/LoginController';
 import authMiddlewares from './app/middlewares/auth';
+import multerUpImgUsers from './app/middlewares/uploadImgUser';
+
 
 const routes = new Router();
+const uploadImgUser = multer(multerUpImgUsers);
 
 routes.get('/users', UserController.index);
 routes.get('/users/:id', UserController.show);
@@ -16,6 +22,7 @@ routes.delete('/users/:id', authMiddlewares, UserController.delete);
 
 routes.get('/perfil', authMiddlewares, PerfilController.show);
 routes.put('/perfil', authMiddlewares, PerfilController.update);
+routes.put('/perfil-img', authMiddlewares, uploadImgUser.single('file'), PerfilImagemController.update);
 
 
 routes.post('/login', LoginController.store)
