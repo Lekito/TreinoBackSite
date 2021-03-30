18d7be6dea17a5c45a43cbfff500c431c574051b
Alex Ribeiro Alves <alexlekito075@gmail.com>
2021-03-30 11:47:58 -0300

    Update PerfilImagemController.js
    Salvando os dados da imagem e mostrando na tela.

--- src/app/controllers/PerfilImagemController.js
+++ src/app/controllers/PerfilImagemController.js
@@ -1,7 +1,26 @@
+import User from '../models/User';
 import fs from 'fs';
 
 class PerfilImagemController{
     async update(require, response){
+        console.log(require.file);
+
+        const dadosImagem = {
+            originalName: require.file.originalname,
+            fileName: require.file.filename
+        }
+
+        await User.findOne({_id: require.userId}, '_id fileName').then((user) => {
+            console.log(user);
+        }).catch((err) => {
+            return response.status(400).json({
+                error: true,
+                code: 128,
+                message: "Erro: Não foi possível executar a solicitação!"
+            })
+        })
+
+        console.log(dadosImagem);
         return response.json({
             error: false,
             message: "Upload img user"
